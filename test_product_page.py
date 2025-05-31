515899dd835c0c0e4a6e65da59e3194e129a2754
Margo <margo@Margos-MacBook-Pro.local>
2025-05-31 13:59:14 +0200

    added parametrization

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,22 @@
 from .pages.product_page import ProductPageLocators, ProductPage
 from .pages.base_page import BasePage
+import pytest
 
-link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('offer', [
+    "offer0",
+    "offer1",
+    "offer2",
+    "offer3",
+    "offer4",
+    "offer5",
+    "offer6",
+    pytest.param("offer7", marks=pytest.mark.xfail),
+    "offer8",
+    "offer9"
+])
 
-def test_guest_can_add_product_to_basket(browser):
+def test_guest_can_add_product_to_basket(browser, offer):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo={offer}"
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.add_to_basket()
